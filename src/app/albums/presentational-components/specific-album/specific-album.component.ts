2df9060a05efd1740a66b983888ed285b6197d2c
Lucas Miranda <lucasgmm@usp.br>
2022-05-02 17:32:02 -0300

    Added truncation in specific album content

--- src/app/albums/presentational-components/specific-album/specific-album.component.ts
+++ src/app/albums/presentational-components/specific-album/specific-album.component.ts
@@ -15,22 +15,47 @@ export class SpecificAlbumComponent implements OnInit {
     this.artistsNames = this.getArtistsNames();
 
     // gets the album's cover art url
-    this.coverArtUrl = this.getCoverArtUrl(this.album.id!);    
+    this.coverArtUrl = this.getCoverArtUrl(this.album.id!);
+
+    // gets the album's title
+    this.albumTitle = this.getAlbumTitle();
   }
 
+
+  // returns the artists names separated by commas
+  // (it's truncated if it's too long)
   getArtistsNames(): string {
-    return this.album.artists.reduce(
+    // if the artist's name is longer than maxChars, it's truncated
+    const maxChars = 15;
+
+    // get the names separated by commas
+    const artistsNames = this.album.artists.reduce(
       (names, artist) =>
         names === '' ? artist.name : names + ', ' + artist.name,
       ''
     );
+
+    return artistsNames.length === maxChars
+      ? artistsNames
+      : artistsNames.substring(0, maxChars);
   }
 
-    // get album cover art url
-    getCoverArtUrl(albumId: string): string{
-      const apiUrl =  environment.apiUrl;
-      return `${apiUrl}/albums/${albumId}/cover`
-    }
+  // returns the album's title
+  // (it's truncated if it's too long)
+  getAlbumTitle(): string {
+    // if the title is longer than maxChars, it's truncated
+    const maxChars = 15;
+
+    return this.album.title.length === maxChars
+      ? this.album.title
+      : this.album.title.substring(0, maxChars) + '...';
+  }
+
+  // get album cover art url
+  getCoverArtUrl(albumId: string): string {
+    const apiUrl = environment.apiUrl;
+    return `${apiUrl}/albums/${albumId}/cover`;
+  }
 
   @Input()
   album!: Album;
@@ -38,4 +63,6 @@ export class SpecificAlbumComponent implements OnInit {
   artistsNames: string = '';
 
   coverArtUrl: string = '';
+
+  albumTitle: string = '';
 }
