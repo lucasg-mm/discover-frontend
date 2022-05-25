137ace99f39c446e979b99d3ae9ee30f1c91cb7d
Lucas Miranda <lucasgmm@usp.br>
2022-05-25 16:32:38 -0300

    Changed album-paginator component tag to <app-paginator>

--- src/app/shared/presentational-components/album-paginator/album-paginator/album-paginator.component.ts
+++ src/app/shared/presentational-components/album-paginator/album-paginator/album-paginator.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-album-paginator',
+  selector: 'app-paginator',
   templateUrl: './album-paginator.component.html',
   styleUrls: ['./album-paginator.component.css'],
 })
