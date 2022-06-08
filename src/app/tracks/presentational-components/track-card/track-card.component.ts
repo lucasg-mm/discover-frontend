6b2714db2c25360ac5a79076004d64d72c79b9de
Lucas Miranda <lucasgmm@usp.br>
2022-06-08 13:26:48 -0300

    Added artists names to track card

--- src/app/tracks/presentational-components/track-card/track-card.component.ts
+++ src/app/tracks/presentational-components/track-card/track-card.component.ts
@@ -15,16 +15,46 @@ export class TrackCardComponent implements OnInit {
 
   formattedTrackName: string = '';
 
+  artistsNames: string = '';
+
   constructor(private albumService: AlbumsService) {}
 
   ngOnInit(): void {
     this.formattedTrackName = this.getFormattedTrackName();
-    if(this.track.album){
-      this.trackCoverUrl = this.albumService.getCoverArtUrl(this.track.album.id!);
-    }
-    else{
+
+    // gets the artists names from the array
+    this.artistsNames = this.getArtistsNames();
+
+    if (this.track.album) {
+      this.trackCoverUrl = this.albumService.getCoverArtUrl(
+        this.track.album.id!
+      );
+    } else {
       this.insertDefaultTrackCover();
     }
+  }
+
+  // returns the artists names separated by commas
+  // (it's truncated if it's too long)
+  getArtistsNames(): string {
+    // if the artist's name is longer than maxChars, it's truncated
+    const maxChars = 15;
+
+    // get the names separated by commas
+    let tracksNames;
+    if (this.track.artists?.length) {
+      tracksNames = this.track.artists.reduce(
+        (names, artist) =>
+          names === '' ? artist.name : names + ', ' + artist.name,
+        ''
+      );
+    } else {
+      tracksNames = '&nbsp'; // renders white space if there are no artists in the album
+    }
+
+    return tracksNames.length <= maxChars
+      ? tracksNames
+      : tracksNames.substring(0, maxChars) + '...';
   }
 
   // returns the track's name (truncated if it's too long)
